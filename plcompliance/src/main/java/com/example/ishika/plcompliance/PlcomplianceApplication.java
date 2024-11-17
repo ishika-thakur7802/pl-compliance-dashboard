72a98695dcfd3f3fcaadf8c69ea7d0c024604871
Ishika Thakur <ishikathakur0708@gmail.com>
2024-11-17 12:17:56 +0530

    Added spring initial files

---
+++ plcompliance/src/main/java/com/example/ishika/plcompliance/PlcomplianceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ishika.plcompliance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlcomplianceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlcomplianceApplication.class, args);
+	}
+
+}
---
+++ plcompliance/src/test/java/com/example/ishika/plcompliance/PlcomplianceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ishika.plcompliance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlcomplianceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
